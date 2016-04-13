a8c6417bf4175a899e48055a3900c71df91c5f30
Greg McIntyre <nocolong@gmail.com>
2016-04-13 13:21:12 -0700

    initial commit

---
+++ RottenAPI/RottenAPI.Web/js/app.js
@@ -0,0 +1,13 @@
+﻿angular.module("rottenApi", [])
+    .service("movieService", function ($http) {
+        return {
+            getMovies: function () {
+                return $http.jsonp("http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=frp6quchv3mhrwpmtb9sq7sy&callback=JSON_CALLBACK");
+            }
+        }
+    })
+    .controller("indexController", function ($scope, movieService) {
+        movieService.getMovies().success(function (data) {
+            $scope.movies = data;
+        });
+    })
